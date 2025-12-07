8e0764dd5c314ea943f59cd21adf6c032a7a7128
Reagan-0 <re332@live.mdx.ac.uk>
2025-12-07 22:11:26 +0400

    Add MongoDB Atlas connection

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-// Week 6b Project - Backend
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
+var { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-// middleware
+var uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.error('MONGODB_URI not set');
+  process.exit(1);
+}
+
+var client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+var db;
+var lessonsCollection;
+var ordersCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    db = client.db(process.env.DB_NAME || 'lessonsdb');
+    lessonsCollection = db.collection('lesson');
+    ordersCollection = db.collection('order');
+    console.log('Collections ready');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+connectDB();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.set('json spaces', 3);
 
-// Week 6b Routing pt2/index.js pattern - logger middleware
 app.use((req, res, next) => {
   console.log("In comes a request to: " + req.url);
   next();
 });
 
-// Week 6b Routing pt2/index.js pattern - start server
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
